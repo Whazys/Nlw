03722d55b12b2e248eeed0e19798857b63b1fb83
whazys <santos_rafa1973@hotmail.com>
2021-11-16 19:58:34 -0300

    Nlw

--- letmeask/src/pages/NewRoom.tsx
+++ letmeask/src/pages/NewRoom.tsx
@@ -1,62 +1,61 @@
-import illustrationImg from '../assets/images/illustration.svg';
-import logoImg from '../assets/images/logo.svg';
-import '../styles/auth.scss';
-import { Button } from '../components/Button'
-import { Link, useHistory } from 'react-router-dom'
 import { FormEvent, useState } from 'react'
-// import { useContext } from 'react';
-// import { AuthContext } from '../contexts/AuthContext'
-// import { useAuth } from '../hooks/useAuth';
-import {database} from '../services/firebase'
-import { useAuth } from '../hooks/useAuth'
+import { Link, useHistory } from 'react-router-dom'
+
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg';
+
+import { Button } from '../components/Button';
+import { database } from '../services/firebase';
+import { useAuth } from '../hooks/useAuth';
+
+import '../styles/auth.scss';
 
 export function NewRoom() {
   const { user } = useAuth()
   const history = useHistory()
-  const [newRoom, setNewRoom] = useState ('')
+  const [newRoom, setNewRoom] = useState('');
 
-  async function handleCreateRoom(event: FormEvent){
-    event.preventDefault()
-    
-    if(newRoom.trim() === '') {
-      return 
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (newRoom.trim() === '') {
+      return;
     }
-    const rommRef = database.ref('rooms')
 
-    const firebaseRoom = await rommRef.push({
+    const roomRef = database.ref('rooms');
+
+    const firebaseRoom = await roomRef.push({
       title: newRoom,
-      authorId: user?.id
+      authorId: user?.id,
     })
+
     history.push(`/rooms/${firebaseRoom.key}`)
   }
 
-  
-  
   return (
     <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
-        <strong>Crie Salas de Q&amp;A ao vivo</strong>
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
-      <main >
+      <main>
         <div className="main-content">
-          <img src={logoImg} alt="LetmeAsk" />
-          
+          <img src={logoImg} alt="Letmeask" />
           <h2>Criar uma nova sala</h2>
           <form onSubmit={handleCreateRoom}>
-            <input
+            <input 
               type="text"
-              placeholder="Nome da Sala"
+              placeholder="Nome da sala"
               onChange={event => setNewRoom(event.target.value)}
               value={newRoom}
             />
             <Button type="submit">
-              Criar Sala
+              Criar sala
             </Button>
           </form>
           <p>
-            Quer  entrar em uma sala existente? <Link to="/">clique aqui</Link>
+            Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
           </p>
         </div>
       </main>
